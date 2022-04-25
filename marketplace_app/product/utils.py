476f570b7970cfd2db995ec028acfd179645229f
Your Name <you@example.com>
2022-04-25 13:40:34 +0500

    shop_008 create base application product models for working with discount

---
+++ marketplace_app/product/utils.py
@@ -0,0 +1,15 @@
+import os.path
+
+
+def category_icon_path(instance, filename) -> str:
+    extension = os.path.splitext(filename)
+    return "category/{id}-{title}.{extension}".format(
+        id=instance.id, title=instance.title, extension=extension
+    )
+
+
+def product_image_path(instance, filename) -> str:
+    extension = os.path.splitext(filename)
+    return "product/{id}.{extension}".format(
+        id=instance.id, extension=extension
+    )
