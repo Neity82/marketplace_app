df61f38c4261086c10ca186391115c4f21c8e313
Evgeniy Albrant <skizee@yandex.ru>
2022-04-26 20:14:09 +0700

    comment Shop
    in settings and urls

--- marketplace_app/marketplace_app/settings.py
+++ marketplace_app/marketplace_app/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    #'shop',
     'info',
 ]
 
