1b20beab149c1526e21bf7c77ac34aa4cc8b84f3
denis <denislegrace@yandex.ru>
2022-05-23 11:25:30 +0500

    shop_039 - Fix view

--- marketplace_app/info/views.py
+++ marketplace_app/info/views.py
@@ -45,7 +45,9 @@ def get_addition_info(request_info) -> Dict[str, str]:
         'meta_title': '',
         'meta_description': '',
     }
-    if 'pk' in request_info.kwargs:
+    if 'pk' in request_info.kwargs and \
+            app_name in addition_info_map and \
+            url_name in addition_info_map[app_name]:
         pk = request_info.kwargs['pk']
         addition_info = addition_info_map[app_name][url_name](pk)
     return addition_info
