706c35cae4ece7cbffd6b3eeff303f21e8821c8d
alandzigkaev <ya.dzhigkaev@yandex.ru>
2022-06-13 19:53:39 +0300

    shop_053 Change celery task

--- marketplace_app/payments/services.py
+++ marketplace_app/payments/services.py
@@ -17,7 +17,12 @@ def check_payments_statuses():
 
     if payments:
         for payment in payments:
-            if random.randint(1, 5) == 5:
+            acquirer_resp = random.randint(1, 5)
+
+            if acquirer_resp in (3, 4):
+                payment.status = "success"
+
+            if acquirer_resp == 5:
                 payment.status = "error"
                 payment.message = MESSAGES["error"][random.randint(0, len(MESSAGES["error"]) - 1)]
 
