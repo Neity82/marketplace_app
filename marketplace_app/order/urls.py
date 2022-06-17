3b21a8bbf3246ede5b1b8a2e57bfab8fe8216054
aleksandr <karpov.a1exandr@yandex.ru>
2022-06-17 15:45:20 +0300

    shop_044 - оформление заказа (кебаб кейс для урлов приложения order)

--- marketplace_app/order/urls.py
+++ marketplace_app/order/urls.py
@@ -11,13 +11,13 @@ order_forms = getattr(views.OrderView, "order_forms", list())
 
 urlpatterns = [
     path("cart/", views.CartView.as_view(), name="cart"),
-    path("cart/add/<int:pk>", views.AddToCartView.as_view(), name="add_to_cart"),
+    path("cart/add/<int:pk>", views.AddToCartView.as_view(), name="add-to-cart"),
     path(
         "cart/remove/<int:pk>",
         views.RemoveFromCartView.as_view(),
-        name="remove_from_cart",
+        name="remove-from-cart",
     ),
-    path("order/<int:pk>/", views.OrderDetail.as_view(), name="order_detail"),
+    path("order/<int:pk>/", views.OrderDetail.as_view(), name="order-detail"),
     path(
         "order/",
         views.OrderView.as_view(order_forms),
@@ -25,4 +25,3 @@ urlpatterns = [
     ),
     path("order/<int:pk>/payment/", views.PaymentView.as_view(), name="payment"),
 ]
-
