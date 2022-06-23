92ac4e9e99780b59b7455e27c890ecedb8142452
aleksandr <karpov.a1exandr@yandex.ru>
2022-06-24 01:51:31 +0300

    shop_033 - order рефакторинг

--- marketplace_app/order/mixins.py
+++ marketplace_app/order/mixins.py
@@ -1,9 +1,25 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.views import View
 
 from order.models import Cart
 from order.utils import SUCCESS_RESPONSE_TYPE, WARNING_RESPONSE_TYPE
 from product.models import Stock
 from user.models import CustomUser
+
+
+def cart_init_data(request: WSGIRequest) -> dict:
+    """
+    Обертка для получения пользователя и устройства из реквеста
+    :param request: wsgi реквест django
+    :return: dict[user, device]
+    """
+    user = getattr(request, "user", None)
+    device = request.COOKIES.get("device", None)
+    assert user, "can't get user from request!"
+    return {
+        'user': user,
+        'device': device
+    }
 
 
 class CartMixin(View):
@@ -20,12 +36,12 @@ class CartMixin(View):
 
     def get_cart(self) -> Cart:
         """Получение объекта корзины из реквеста пользователя"""
-        return self.cart_model.get_cart(self.request)
+        return self.cart_model.get_cart(**cart_init_data(self.request))
 
     def get_cart_pk(self) -> int:
         """Получение id объекта корзины"""
         cart = self.get_cart()
-        return getattr(cart, "id")
+        return getattr(cart, "id", None)
 
     def get_stock(self, pk: int) -> Stock:
         """Получение объекта 'Складской единицы' по id"""
