3201bb5b5f824af234acfaa1234a53dfa8eff30b
Natalia Kurdachenkova <nata_111082@mail.ru>
2022-07-05 13:45:13 +0300

    Shop_063 - banner and top-product  cache

--- marketplace_app/info/context_processors.py
+++ marketplace_app/info/context_processors.py
@@ -9,17 +9,10 @@ def cache_time(request: WSGIRequest) -> dict:
     product_cache = Settings.objects.filter(name="product_list_cache_time").first()
     top_product_cache = Settings.objects.filter(name="top_product_list_cache_time").first()
     banner_cache = Settings.objects.filter(name="banner_list_cache_time").first()
+
     return {
-        "category_list_cache_time": category_cache.value
-        if category_cache
-        else DEFAULT_CACHE_TIME,
-        "product_list_cache_time": product_cache.value
-        if product_cache
-        else DEFAULT_CACHE_TIME,
-        "top_product_list_cache_time": top_product_cache.value
-        if top_product_cache
-        else DEFAULT_CACHE_TIME,
-        "banner_list_cache_time": banner_cache.value
-        if banner_cache
-        else DEFAULT_CACHE_TIME,
+        "category_list_cache_time": category_cache.value if category_cache else DEFAULT_CACHE_TIME,
+        "product_list_cache_time": product_cache.value if product_cache else DEFAULT_CACHE_TIME,
+        "top_product_list_cache_time": top_product_cache.value if top_product_cache else DEFAULT_CACHE_TIME,
+        "banner_list_cache_time": banner_cache.value if banner_cache else DEFAULT_CACHE_TIME,
     }
