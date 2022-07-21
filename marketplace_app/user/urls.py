a2632622d0859cc48ff7334b382f00c692bafe9c
denis <denislegrace@yandex.ru>
2022-07-21 13:24:38 +0500

    shop_070 - refactoring

--- marketplace_app/user/urls.py
+++ marketplace_app/user/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
 
-    path('password_reset/', views.PasswordResetViewCustom.as_view(), name='password_reset'
-         ),
+    path('password_reset/', views.PasswordResetViewCustom.as_view(),
+         name='password_reset',),
     path('password_reset/done/', views.PasswordResetDoneViewCustom.as_view(),
          name='password_reset_done'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),
