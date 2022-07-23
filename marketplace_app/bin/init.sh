a7a6d46f66b4f5c58dfe4f308fdd17b65317b0f8
aleksandr <karpov.a1exandr@yandex.ru>
2022-07-23 18:44:02 +0300

    нет задачи: актуализация фикстур

--- marketplace_app/bin/init.sh
+++ marketplace_app/bin/init.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py loaddata settings.json seo.json banners.json discount.json users.json shop.json product.json orders.json atribute.json task.json
+python manage.py loaddata settings.json seo.json banners.json discount.json users.json shop.json product.json orders.json atribute.json task.json productimages.json
